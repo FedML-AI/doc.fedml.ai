49e211877cd1c973391cae61468551acb9668d17
alaydshah <alay11shah@gmail.com>
2024-09-03 23:58:18 +0000

    Fix Render Token User Input

--- static/render/shell/node_bind.sh
+++ static/render/shell/node_bind.sh
@@ -2,23 +2,13 @@
 set -e
 
 # Check if an argument was provided
-if [ $# -eq 0 ]; then
-    echo -e "\033[1;35m🔑 Enter your render token:\033[0m"
-    read render_auth_token
-else
-    render_auth_token="$1"
+echo -e "\033[1;35m🔑 Enter your render token:\033[0m"
+read render_auth_token
+
+if [ -z "$render_auth_token" ]; then
+    echo -e "\033[1;31m\u2717 Error: Render Auth Token is missing. Kindly execute the last command again, and enter Render Auth Token when prompted\033[0m"
+    exit 1
 fi
-
-# Function to validate the Render auth token
-validate_render_token() {
-    if [ -z "$render_auth_token" ]; then
-        echo -e "\033[1;31m\u2717 Error: Render Auth Token is missing. Kindly execute the last command again, and enter Render Auth Token when prompted\033[0m"
-        exit 1
-    fi
-}
-
-# Call the validation function
-validate_render_token
 
 cleanup() {
     # Returning to the original directory
