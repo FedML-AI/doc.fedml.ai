6d60b0c09e08e0e232f891aa5b386cbc39184366
alaydshah <alay11shah@gmail.com>
2024-09-04 23:52:53 +0000

    Download files in hidden tmp folder

--- static/render/shell/node_bind.sh
+++ static/render/shell/node_bind.sh
@@ -27,24 +27,7 @@ cleanup() {
 
 trap 'cleanup; echo -e "\e[31m✘ Failed to link render token to the node. Please retry the binding process again from the beginning.\e[0m"' ERR
 
-# Creating the tmp folder or removing it if it already exists
-tmp_folder=".render-fedml-bind"
-
-# Creating the tmp folder or removing it if it already exists
-if [ -d "$tmp_folder" ]; then
-    rm -rf "$tmp_folder"
-fi
-mkdir "$tmp_folder"
-
-# Moving into the tmp folder
-cd "$tmp_folder"
 sudo wget -q https://doc.fedml.ai/render/python/render.py && sudo chmod +x render.py
 python3 render.py "$render_auth_token"
-
-# Returning to the original directory
-cd ..
-
-# Removing the tmp folder and its contents
-rm -rf "$tmp_folder"
 
 echo -e "\033[1;35m🚀 Your node was successfully binded to TensorOpera Platform!\033[0m"
